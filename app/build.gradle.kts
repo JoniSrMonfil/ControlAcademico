8dfb0e7f9e97a27cec6df0c1e8c2c50a99443658
JoniSrMonfil <jonathan_adidas10@hotmail.com>
2026-03-18 01:50:35 -0600

    Cambios para el UX - 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.google.firebase:firebase-storage-ktx")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
